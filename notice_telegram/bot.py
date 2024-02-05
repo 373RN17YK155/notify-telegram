f479aa518a9b08f437fd067fab533f8623e55ca9
373RN17YK155 <vladislav.burko.worker@gmail.com>
2024-02-05 10:01:28 +0300

    downgrade projext python version make bot from examplea

--- notice_telegram/bot.py
+++ notice_telegram/bot.py
@@ -1,47 +1,148 @@
-import os
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import os
+import sqlite3
 
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
-# App contsants
+# Env 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TARGET_URL = os.getenv("TARGET_URL")
-
-
-# Check constants
-if not TELEGRAM_BOT_TOKEN:
-    exit("Specify TELEGRAM_BOT_TOKEN env variable")
-
-if not TARGET_URL:
-    exit("Specify TARGET_URL env variable")
 
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+SUBSCRIBE = 0
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends explanation on how to use the bot."""
+    await update.message.reply_text("Hi! Use /set <seconds> to set a timer")
 
 
-# Define handlers
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привет")
-    
-async def subscribe(update, context):
-    # Get the parameters passed with the command
-    # For example, /custom_command parameter1 parameter2
-    parameters = context.args
-    if not parameters:
-        await update.message.reply_text('No parameters provided for the command.')
-    else:
-        await update.message.reply_text(f'Command received with parameters: {", ".join(parameters)}')
+async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the alarm message."""
+    job = context.job
+    await context.bot.send_message(job.chat_id, text=f"Beep! {job.data} seconds are over!")
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Sorry we can not go back to future!")
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        text = "Timer successfully set!"
+        if job_removed:
+            text += " Old one was removed."
+        await update.effective_message.reply_text(text)
+
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Usage: /set <seconds>")
+
+
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await update.message.reply_text(text)
+
+
+async def make_sub(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Make subscribtion on search"""
+    await update.message.reply_text(
+        "Hi! Insert cookie for make query\n"
+        "Send /cancel to stop action."
+    )
+    return SUBSCRIBE
+
+
+async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Make subscribtion on search"""
+    user = update.message.from_user
+    cookie = update.message.text 
+
+    con = sqlite3.connect("./db.sqlite3")
+    cur = con.cursor()
     
-    conversation_handler = ConversationHandler()
-    
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler("subscribe", subscribe))
-    
-    application.run_polling()
+    data = ({"cookie"})
+    cur.execute("INSERT INTO subscribtion VALUES(:cookie)", data)
+    con.commit()  # Remember to commit thetransaction after executing INSERT.
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels from conversation handler."""
+
+    return ConversationHandler.END
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    application.add_handler(CommandHandler(["start", "help"], start))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
+    application.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler("subscribe", make_sub)],
+            states={
+                SUBSCRIBE:[
+                    MessageHandler(filters.TEXT & ~filters.COMMAND,subscribe),
+                ],
+            },
+            fallbacks=[CommandHandler("cancel", cancel)],
+        )
+    ) # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == "__main__":
+    main()
